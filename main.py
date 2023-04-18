28598c661db71a5b59ec2b0fcdbf8e0bdcb792d2
Jorik-Laptop <mastakcompany@gmail.com>
2023-04-18 13:24:54 +0700

    Corrected the notes

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import random
 import shutil
-from pathlib import Path
 import os
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
+
+
+def processing_errors(response):
+    response.raise_for_status()
+    response = response.json()
+    if 'error' in response:
+        error_description = response['error']
+        error_code = error_description['error_code']
+        error_msg = error_description['error_msg']
+        raise requests.HTTPError(error_code, error_msg)
+    return response
 
 
 def get_random_comic_number():
@@ -30,12 +41,8 @@ def download_image(url, path, name):
     Path(path).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
-    with open(f'{path}/{name}.jpg', 'wb') as file:
+    with open(f'{path/name}.jpg', 'wb') as file:
         file.write(response.content)
-
-
-def remove_images(path):
-    shutil.rmtree(path)
 
 
 def get_wall_upload_server(group_id, api_version, access_token):
@@ -45,54 +52,36 @@ def get_wall_upload_server(group_id, api_version, access_token):
         'group_id': group_id,
         'v': api_version
     }
-    response = requests.get(
-        api_endpoint,
-        params=params
-    )
-    response.raise_for_status()
-    photo = response.json()["response"]
-    album_id = photo["album_id"]
-    upload_url = photo["upload_url"]
-    user_id = photo["user_id"]
-
-    return album_id, upload_url, user_id
+    response = processing_errors(requests.get(api_endpoint, params=params))
+    return response["response"]["upload_url"]
 
 
 def upload_photo(upload_url, image_path, image_name):
-    with open(f'{image_path}/{image_name}.jpg', 'rb') as file:
+    with open(f'{image_path/image_name}.jpg', 'rb') as file:
         files = {
             'photo': file
         }
-        response = requests.post(
-            url=upload_url,
-            files=files
-        )
-        response.raise_for_status()
-        server = response.json()["server"]
-        photo = response.json()["photo"]
-        photo_hash = response.json()["hash"]
+        response = processing_errors(requests.post(url=upload_url, files=files))
+    server = response["server"]
+    photo = response["photo"]
+    photo_hash = response["hash"]
     return server, photo, photo_hash
 
 
-def save_photo_on_wall(access_token, client_id, group_id, photo, server, photo_hash):
+def save_photo_on_wall(access_token, group_id, photo, server, photo_hash):
     api_endpoint = f'https://api.vk.com/method/photos.saveWallPhoto'
     params = {
         'access_token': access_token,
-        'user_id': client_id,
         'group_id': group_id,
         'photo': photo,
         'server': server,
         'hash': photo_hash,
         'v': api_version,
     }
-    response = requests.post(
-        api_endpoint,
-        params=params
-    )
-    response.raise_for_status()
-    media = response.json()["response"]
-    media_id = media[0]["id"]
-    owner_id = media[0]["owner_id"]
+    response = processing_errors(requests.post(api_endpoint, params=params))
+    media = response["response"][0]
+    media_id = media["id"]
+    owner_id = media["owner_id"]
 
     return media_id, owner_id
 
@@ -108,39 +97,43 @@ def publish_photo_on_wall(access_token, group_id, caption, owner_id, media_id, a
         'attachments': f'photo{owner_id}_{media_id}',
         'v': api_version
     }
-    response = requests.post(
-        api_save_photo,
-        params=params
-    )
-    response.raise_for_status()
+    processing_errors(requests.post(api_save_photo, params=params))
 
 
 if __name__ == '__main__':
     api_version = '5.131'
     image_name = 'comics'
-    path = './Files/'
+    path = Path.cwd()/'Files'
     load_dotenv()
     access_token = os.environ['VK_ACCESS_TOKEN']
     client_id = os.environ['VK_CLIENT_ID']
     group_id = os.environ['VK_GROUP_ID']
-    caption = download_random_comic(image_name, path)
-    album_id, upload_url, user_id = get_wall_upload_server(group_id, api_version, access_token)
-    server, photo, photo_hash = upload_photo(upload_url, path, image_name)
-    media_id, owner_id = save_photo_on_wall(
-        access_token,
-        client_id,
-        group_id,
-        photo,
-        server,
-        photo_hash
-    )
-    publish_photo_on_wall(
-        access_token,
-        group_id,
-        caption,
-        owner_id,
-        media_id,
-        api_version
-    )
-
-    remove_images(path)
+    try:
+        caption = download_random_comic(image_name, path)
+        upload_url = get_wall_upload_server(
+            group_id,
+            api_version,
+            access_token
+        )
+        server, photo, photo_hash = upload_photo(
+            upload_url,
+            path,
+            image_name
+        )
+        media_id, owner_id = save_photo_on_wall(
+            access_token,
+            group_id,
+            photo,
+            server,
+            photo_hash
+        )
+        publish_photo_on_wall(
+            access_token,
+            group_id,
+            caption,
+            owner_id,
+            media_id,
+            api_version
+        )
+    finally:
+        shutil.rmtree(path)
